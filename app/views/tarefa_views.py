31450035567785965f766151b40499dbbeaa452b
viniciusdss <viniciusdss@gmail.com>
2020-04-30 18:54:58 -0300

    Autenticação_modificações1

--- app/views/tarefa_views.py
+++ app/views/tarefa_views.py
@@ -1,14 +1,18 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from .forms import TarefaForm
-from .entidades.tarefa import Tarefa
-from .services import tarefa_service
+from ..forms import TarefaForm
+from ..entidades.tarefa import Tarefa
+from ..services import tarefa_service
 
 # Create your views here.
+
+@login_required()
 def listar_tarefas(request):
         tarefas=tarefa_service.listar_tarefas()
         nome_tarefa="Assistir Mr. Robot"
         return render(request, 'tarefas/listar_tarefas.html',{"tarefas": tarefas})
 
+@login_required()
 def cadastrar_tarefa(request):
         if request.method=="POST":
             form_tarefa=TarefaForm(request.POST)
@@ -24,6 +28,7 @@ def cadastrar_tarefa(request):
             form_tarefa=TarefaForm()
         return render(request, 'tarefas/form_tarefa.html',{"form_tarefa":form_tarefa})
 
+@login_required()
 def editar_tarefa(request,id):
     tarefa_db=tarefa_service.listar_tarefa_id(id)
     form_tarefa=TarefaForm(request.POST or None, instance=tarefa_db)
@@ -37,6 +42,7 @@ def editar_tarefa(request,id):
         return redirect('listar_tarefas')
     return render(request, 'tarefas/form_tarefa.html', {"form_tarefa": form_tarefa})
 
+@login_required()
 def remover_tarefa(request, id):
     tarefa_bd=tarefa_service.listar_tarefa_id(id)
     if request.method=="POST":
